6d696a5e93e4d4add4748226c71caca52e941778
MichelDesktop <misoares.777@gmail.com>
2015-12-28 21:10:31 -0200

    Metodos get set class Usuario

--- src/br/com/myplace/entidades/Usuario.java
+++ src/br/com/myplace/entidades/Usuario.java
@@ -9,6 +9,31 @@ public class Usuario {
 	private Date dataNascimento;
 	private String email;
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 	
 	
 
