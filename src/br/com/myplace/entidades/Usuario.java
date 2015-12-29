463ac0d7ae07490586c2894a02fd07ae55ff9cb4
MICHEL DE LIMA SOARES <MSOARES@AGECOM.CORP>
2015-12-29 15:58:58 -0200

    add construtores classe Usuario

--- src/br/com/myplace/entidades/Usuario.java
+++ src/br/com/myplace/entidades/Usuario.java
@@ -3,38 +3,53 @@ package br.com.myplace.entidades;
 import java.util.Date;
 
 public class Usuario {
-	
+
 	private Long id;
 	private String nomeCompleto;
 	private Date dataNascimento;
 	private String email;
-	
+
+	public Usuario() {
+	}
+
+	public Usuario(Long id, String nomeCompleto, Date dataNascimento, String email) {
+		super();
+		this.id = id;
+		this.nomeCompleto = nomeCompleto;
+		this.dataNascimento = dataNascimento;
+		this.email = email;
+	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNomeCompleto() {
 		return nomeCompleto;
 	}
+
 	public void setNomeCompleto(String nomeCompleto) {
 		this.nomeCompleto = nomeCompleto;
 	}
+
 	public Date getDataNascimento() {
 		return dataNascimento;
 	}
+
 	public void setDataNascimento(Date dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
-	
 
 }
