f31662836015ff2de159d7d28d1b0a8bd5c89715
MichelDesktop <misoares.777@gmail.com>
2016-01-02 22:04:19 -0200

    trabalhando no layout com FACELETS

---
+++ src/br/com/myplace/MB/CadastroMB.java
@@ -0,0 +1,14 @@
+package br.com.myplace.MB;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class CadastroMB {
+
+	
+	public String irParaTelaCadastro(){
+		return "cadastro_usuario";
+	}
+	
+	
+}
